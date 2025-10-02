cd2ffd8531f3ab3ebb4d6bdbb0f2e62fffc48011
Peter Takhar <parminderst@gmail.com>
2025-10-01 21:50:18 -0400

    add logic to display bytes size of file

---
+++ README.md
@@ -0,0 +1,4 @@
+# WC Tool - NodeJS Style
+
+Just a rewrite of wc from unix.
+Will match same behaviour as wc unless stated otherwise.
--- wc.js
+++ wc.js
@@ -1,5 +1,30 @@
-const { argv } = require("node:process");
+// wc - prints newline, word, byte for each File and a total line if more than one FILE is specified. 
+//      with no FILE, or when FILE is -, read standard input
+//      always in this word: newline, word, character, byte, maximum line length
 
+
+const { argv } = require("node:process");
+const fs = require("node:fs");
+
+let fileName = null;
+
+
+//re-write to process command line statements
 argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
+    if (index == 2) fileName = val;
 });
+
+if (fileName == null) {
+    throw new Error("No filename provided. File name is the first argument.");
+}
+
+console.log(`You have inputted this file name: ${fileName}`);
+
+
+fs.readFile(fileName, 'utf8', (err,data) => {
+    if (err) throw err;
+    
+    const blob = new Blob([data]);
+    console.log(`${blob.size} ${fileName}`);
+});
+
