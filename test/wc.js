56604cc17e3dec4620b38b988076b5a4adbce7f2
Peter Takhar <parminderst@gmail.com>
2025-10-07 02:32:46 -0400

    add initial stdin handling

--- test/wc.js
+++ test/wc.js
@@ -2,5 +2,4 @@ const test = require("node:test");
 
 test('wc byte count', (t) => {
     const expected = "   7145  58164 339292 342190     78 data/test.txt";
-    
 })
--- wc.js
+++ wc.js
@@ -5,8 +5,9 @@
 
 //TODO:
 // tests, docker, standard input?, flags, error paths
-const { argv } = require("node:process");
+const { argv, stdin: input, stdout: output } = require("node:process");
 const fs = require("node:fs");
+const readLine = require("node:readline");
 
 let fileName = null;
 
@@ -18,22 +19,21 @@ let wordsFlag = false;
 
 //re-write to process command line statements
 argv.forEach((val) => {
-    if (!(val.includes("node") || val.includes("wc.js")))  {
+    if (!(val.includes("node") || val.includes("wc.js"))) {
         if (val.match(/^-/)) {
-    
             val.substring(1).split('').forEach(str => {
                 if (!str.match(/[lcmLw]/)) {
                     throw new Error(`Invalid flag: ${str}`);
                 }
                 if (str.match("l")) {
                     newLineFlag = true;
-                } else if (str.match("c")){
+                } else if (str.match("c")) {
                     bytesFlag = true;
                 } else if (str.match("m")) {
                     charsFlag = true;
                 } else if (str.match("L")) {
                     maxLinesFlag = true;
-                } else {
+                } else if (str.match("w")) {
                     wordsFlag = true;
                 }
             });
@@ -42,37 +42,69 @@ argv.forEach((val) => {
             fileName = val;
         }
     }
-
+    if (fileName == null) fileName = "-";
 });
 
 if (fileName == null) {
     throw new Error("No filename provided. File name is the first argument.");
 }
 
-fs.readFile(fileName, 'utf8', (err, data) => {
-    if (err) throw err;
-    
-    const blob = new Blob([data]);
+if (fileName == '-') {
+    //WHY DID PUTTING ALIAS OF INPUT MAKE THIS WORK???????
+    const rl = readLine.createInterface({ input, output });
+    let linesCount = 0;
+    let wordsCount = 0;
+    let byteCount = 0;
+    rl.on('line', (input) => {
+        linesCount++;
+        wordsCount += input.split(/[\s+]/).length;
+        byteCount += new Blob([input]).size;
+        console.log(`Received: ${input}`);
+        //does not count words or bytes on last line without newline
+    });
 
-    let lines = data.split(/\r\n/g);
-    let maximumLineLength = lines.reduce((acc, curr) => curr.length > acc ? curr.length : acc, 0);
-    
-    //tabs and empty space can seperate sequence of nonwhite space characters
-    let words = lines.filter(line => line.match(/\S+/g)).flatMap(str => str.split(" ")).flatMap(str => str.split(/\t/)).filter(word => word.match(/\S+/g));
-     
-    let charCount = data.split("").reduce((acc, curr) => acc + curr.length, 0);
-    //POSIX def of a line is a sequence of characters that end in a new line
-    //edge case, it is unnecessary to add an extra byte to the last line for newline but its an oddity
+    //there is no EOF signal, using SIGINT instead to handle end of input
+    //trying to build output in previous
+    rl.on('SIGINT', () => {
+        //rl.line? process whats left the line
+        //looks like even though im trying to treat it like a file, stdin doesn't work that way
+        //different model
+        console.log(`${linesCount} ${wordsCount} ${byteCount} ${fileName}`);
+        rl.close();
+    })
+    // rl.question('', (answer) => {
+    //     console.log(`Answer: ${answer}`);
+    // });
 
-    let output = '';
-    if (!newLineFlag && !wordsFlag && !charsFlag && !bytesFlag && !maxLinesFlag) output += `${lines.length - 1} ${words.length} ${blob.size} `
-    if (newLineFlag) output += lines.length - 1 + " ";
-    if (wordsFlag) output += words.length + " ";
-    if (charsFlag) output += charCount + " ";
-    if (bytesFlag) output += blob.size + " ";
-    if (maxLinesFlag) output += maximumLineLength + " "; 
-    console.log(`${output}${fileName}`);
-});
+    // rl.close();
+} else {
+    fs.readFile(fileName, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const blob = new Blob([data]);
+
+        let lines = data.split(/\r\n/g);
+        let maximumLineLength = lines.reduce((acc, curr) => curr.length > acc ? curr.length : acc, 0);
+
+        //tabs and empty space can seperate sequence of nonwhite space characters
+        let words = lines.filter(line => line.match(/\S+/g)).flatMap(str => str.split(" ")).flatMap(str => str.split(/\t/)).filter(word => word.match(/\S+/g));
+
+        let charCount = data.split("").reduce((acc, curr) => acc + curr.length, 0);
+        //POSIX def of a line is a sequence of characters that end in a new line
+        //edge case, it is unnecessary to add an extra byte to the last line for newline but its an oddity
+
+        let output = '';
+        if (!newLineFlag && !wordsFlag && !charsFlag && !bytesFlag && !maxLinesFlag) output += `${lines.length - 1} ${words.length} ${blob.size} `
+        if (newLineFlag) output += lines.length - 1 + " ";
+        if (wordsFlag) output += words.length + " ";
+        if (charsFlag) output += charCount + " ";
+        if (bytesFlag) output += blob.size + " ";
+        if (maxLinesFlag) output += maximumLineLength + " ";
+        console.log(`${output}${fileName}`);
+    });
+
+}
+
 
 
 
